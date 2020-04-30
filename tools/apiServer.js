4efec482bcd6070fb32ddb380f25750b14c6ac77
Alisha Mohanty <alishamohanty1416@gmail.com>
2020-04-30 22:06:44 +0530

    Add scripts & auto-format double-quotes to single-quotes

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -11,15 +11,15 @@ Relevant source code: https://github.com/typicode/json-server/blob/master/src/cl
 */
 
 /* eslint-disable no-console */
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const path = require("path");
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
 // Can pass a limited number of options to this to override (some) defaults. See https://github.com/typicode/json-server#api
 const middlewares = jsonServer.defaults({
   // Display json-server's built in homepage when json-server starts.
-  static: "node_modules/json-server/dist"
+  static: 'node_modules/json-server/dist',
 });
 
 // Set default middlewares (logger, static, cors and no-cache)
@@ -29,7 +29,7 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
 // Simulate delay on all requests
-server.use(function(req, res, next) {
+server.use(function (req, res, next) {
   setTimeout(next, 0);
 });
 
@@ -37,14 +37,14 @@ server.use(function(req, res, next) {
 
 // Add createdAt to all POSTS
 server.use((req, res, next) => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     req.body.createdAt = Date.now();
   }
   // Continue to JSON Server router
   next();
 });
 
-server.post("/courses/", function(req, res, next) {
+server.post('/courses/', function (req, res, next) {
   const error = validateCourse(req.body);
   if (error) {
     res.status(400).send(error);
@@ -68,14 +68,14 @@ server.listen(port, () => {
 // Returns a URL friendly slug
 function createSlug(value) {
   return value
-    .replace(/[^a-z0-9_]+/gi, "-")
-    .replace(/^-|-$/g, "")
+    .replace(/[^a-z0-9_]+/gi, '-')
+    .replace(/^-|-$/g, '')
     .toLowerCase();
 }
 
 function validateCourse(course) {
-  if (!course.title) return "Title is required.";
-  if (!course.authorId) return "Author is required.";
-  if (!course.category) return "Category is required.";
-  return "";
+  if (!course.title) return 'Title is required.';
+  if (!course.authorId) return 'Author is required.';
+  if (!course.category) return 'Category is required.';
+  return '';
 }
