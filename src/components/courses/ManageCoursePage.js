9e21e164a81200cf07e85099645b66180043e7e7
Alisha Mohanty <alishamohanty1416@gmail.com>
2020-05-01 23:49:13 +0530

    Use hooks instead of class components

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,32 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { loadCourses } from '../../redux/actions/courseActions';
 import { loadAuthors } from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
-
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, actions } = this.props;
-
+function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+  useEffect(() => {
     if (courses.length === 0) {
-      actions.loadCourses().catch((error) => {
+      loadCourses().catch((error) => {
         alert('Loading courses failed' + error);
       });
     }
 
     if (authors.length === 0) {
-      actions.loadAuthors().catch((error) => {
+      loadAuthors().catch((error) => {
         alert('Loading authors failed' + error);
       });
     }
-  }
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  }, []);
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 ManageCoursePage.propTypes = {
