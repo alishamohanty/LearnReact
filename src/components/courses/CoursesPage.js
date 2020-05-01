e95b747705b7e9f8ea57c9c3f16dfbf6837ecad3
Alisha Mohanty <alishamohanty1416@gmail.com>
2020-05-01 16:44:50 +0530

    Get all course data in frontend

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
+import CourseList from './CourseList';
 class CoursesPage extends React.Component {
   state = {
     course: {
@@ -18,9 +19,7 @@ class CoursesPage extends React.Component {
     return (
       <>
         <h2>Courses</h2>
-        {this.props.courses.map((course) => (
-          <div key={course.title}>{course.title}</div>
-        ))}
+        <CourseList courses={this.props.courses}></CourseList>
       </>
     );
   }
