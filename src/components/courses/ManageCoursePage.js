697a1e34266b5f596b162cb2a3b5edbefc64b9c3
Alisha Mohanty <alishamohanty1416@gmail.com>
2020-05-08 17:08:41 +0530

    Add client side validation

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -44,16 +44,32 @@ function ManageCoursePage({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = 'Title is required';
+    if (!authorId) errors.author = 'Author is required';
+    if (!category) errors.category = 'Category is required';
+
+    setErrors(errors);
+    //Form is valid if the error object still has no properties...
+    return Object.keys(errors).length === 0;
+  }
+
   function handleSave(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     setSaving(true);
-    saveCourse(course).then(() => {
-      toast.success('Course saved.');
-      history.push('/courses');
-    }).catch((error) => {
-      setSaving(false);
-      setErrors({onSave: error.message});
-    });
+    saveCourse(course)
+      .then(() => {
+        toast.success('Course saved.');
+        history.push('/courses');
+      })
+      .catch((error) => {
+        setSaving(false);
+        setErrors({ onSave: error.message });
+      });
   }
 
   return (
